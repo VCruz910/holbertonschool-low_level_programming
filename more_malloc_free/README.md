7a1115a6f358073bda755fabd6b9f572e27f3b8c
VCruz910 <vscruz910@gmail.com>
2022-06-27 15:02:02 +0000

    C - More Malloc, Free

---
+++ more_malloc_free/README.md
@@ -0,0 +1,11 @@
+##Holberton School Low Level Programming: C - More Malloc, Free
+### Directory Contents
+- main.h: Contains all the prototypes of the functions.
+
+- 0-malloc_checked.c: Allocates memory using malloc.
+
+- 1-string_nconcat.c: Concatenates two strings.
+
+- 2-calloc.c: Allocates memory for an array, using malloc.
+
+- 3-array_range.c: Creates an array of integers.
---
+++ more_malloc_free/main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*Header files*/
+#include <stdio.h>
+#include <assert.h>
+#include <ctype.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/*Function Prototypes*/
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
