663ba17d1664a58be723d2423f03a425fedebb8d
VCruz910 <vscruz910@gmail.com>
2022-07-06 16:27:37 +0000

    C - Variadic Functions

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,13 +1,16 @@
 section .data
 msg db "Hello, World",0xa
+len equ $ - msg
+
 section .text
 global main
 
 main:
-mov rax, 1
-mov rdi, 1
-mov rsi, msg
-mov rdx, 17
+mov eax, 4
+mov ebx, 1
+mov ecx, msg
+mov edx, len
+int 0x80
 syscall
 
 mov rax, 60
