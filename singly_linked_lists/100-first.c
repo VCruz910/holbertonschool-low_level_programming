234ad6db0fdc9daa46a3c2bc518766d85c9e2537
VCruz910 <vscruz910@gmail.com>
2022-07-15 22:44:05 +0000

    C - Singly Linked Lists: Created and tested function (Success).

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * Aesop - Prints before the main function.
+ **/
+void __attribute__ ((constructor)) Aesop()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
